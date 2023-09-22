2ba1e416e6cf953e2e5f4714b608526453a2f1f1
小邝今天学习了嘛 <nolankuang@gmail.com>
2023-09-22 19:30:44 +0800

    事务核心要点

---
+++ spring/spring_transaction/src/main/java/config/config.java
@@ -0,0 +1,55 @@
+package config;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ktrol
+ * @Date: 2023/07/11/16:55
+ */
+
+@Configuration//配置类
+@ComponentScan(basePackages = {"config","dao","service"})//组件扫描
+@EnableTransactionManagement//开启事务
+public class config {
+
+    //创建数据库连接池
+    @Bean
+    public DruidDataSource getDruidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql:///test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    //创建JdbcTemplate对象
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        //到ioc容器中根据类型找到dataSource
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        //注入dataSource
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    //创建事务管理器
+    @Bean
+    public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+       return transactionManager;
+    }
+}
+
